56dcb0124067631ca934749938eb2391f4843c8c
Nani <lakshminarayanamalineni1@gmail.com>
2023-08-10 21:10:51 +0530

    Added all file and modules

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React, {useState} from 'react';
+import quotes from "./quotes"
+import gradient from "./colors"
+function App() {
+  const [Quote, setQuote] = useState("Hey, Welcome Click below to generate a Quote")
+  const [author, setauthor] = useState('nani')
+  const [random, setrandom] = useState(0)
+  const [color, setColor] = useState("#9FA0A8", "#5C7852")
+  const randomgenerate= ()=>{
+    let rint = Math.floor(100*Math.random())
+    setrandom(rint)
+    setQuote(quotes[rint].quote)
+    setColor(gradient[rint].colors)
+    setauthor(quotes[rint].author)
+  }
+  // const changeButton = () =>{
+  //   setauthor('erpuk');
+  //   setQuote('dengei');
+  // }
+  return (
+    <div className="App">
+      <header className="App-header" style={{background: `radial-gradient(${color[1]},${color[0]})`,color:color[0]}}>
+        <div id="quote-box">
+          
+          <h1 id="text">"{Quote}"</h1>
+          <p id="author">-{author}</p>
+          <a href={encodeURI(`http://www.twitter.com/intent/tweet?text=${Quote}`)}id="tweet-quote"> <i style={{color:color[0]}} class="fa fa-twitter"></i></a>
+          <button id="new-quote" onClick={()=>randomgenerate()} style={{background: `radial-gradient(${color[0]},${color[1]})`}}>Generate</button>
+          
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
